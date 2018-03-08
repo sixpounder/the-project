59edc40405e9ed8088e7c38410a409fa2d5e0828
Andrea Coronese <sixpounder@protonmail.com>
2018-03-08 20:11:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express      = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const fs           = require('fs');
+
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  fs.readFile('views/index.html', 'utf-8', (error, text) => {
+    if (error) {
+      res.status(500);
+      res.end();
+    } else {
+      res.set('Content-Type', 'text/html');
+      res.end(text);
+    }
+  });
+});
+
+app.listen(8080);
