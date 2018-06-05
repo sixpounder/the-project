f917a4a2f22cffa582afc83ffa83e8d67dd23672
Andrea <sixpounder@protonmail.com>
2018-06-05 09:55:45 +0200

    socket fix

--- middlewares/socketUser.js
+++ middlewares/socketUser.js
@@ -1,7 +1,7 @@
 const sequelize = require('../models');
 
 module.exports = (socket, next) => {
-  if (socket.request.session.userId) {
+  if (socket.request.session && socket.request.session.userId) {
     sequelize.models.user.findOne({ where: { id: socket.request.session.userId }}).then(user => {
       socket.user = user;
       next();
